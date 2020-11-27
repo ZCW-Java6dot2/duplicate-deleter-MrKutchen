84dbd26ceb7cc7a7ff05799a7ad76d2926b673c0
Peter <pgkutc@gmail.com>
2020-11-27 10:35:03 -0500

    Finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,64 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int quantityToBeDeleted = 0;
+        Integer[] duplicates = countOccurrences();
+        for (Integer duplicate : duplicates) {
+            if (duplicate >= maxNumberOfDuplications) {
+                quantityToBeDeleted++;
+            }
+        }
+        Integer[] result = new Integer[array.length - quantityToBeDeleted];
+        if (result.length == 0) return result;
+        int j=0;
+        for (int i=0; i < duplicates.length; i++){
+            if (duplicates[i] < maxNumberOfDuplications){
+                result[j] = array[i];
+                j++;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int indexToDelete = 0;
+        Integer[] duplicates = countOccurrences();
+        for (Integer duplicate : duplicates) {
+            if (duplicate == exactNumberOfDuplications) {
+                indexToDelete++;
+            }
+        }
+        Integer[] result = new Integer[array.length-indexToDelete];
+        int j=0;
+        for (int i=0; i < duplicates.length; i++){
+            if (duplicates[i] != exactNumberOfDuplications){
+                result[j] = array[i];
+                j++;
+            }
+        }
+        return result;
+    }
+
+    public Integer[] countOccurrences(){
+        Integer[] duplicatesCounter = new Integer[array.length];
+        for (int i=0; i<array.length; i++){
+            int counter = 1;
+            for (int j=0; j<array.length; j++){
+                if (i != j){
+                    if (array[i].equals(array[j])){
+                        counter++;
+                    }
+                }
+            }
+            duplicatesCounter[i] = counter;
+        }
+        return duplicatesCounter;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,66 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int quantityToBeDeleted = 0;
+        Integer[] duplicates = countOccurrences();
+        for (Integer duplicate : duplicates) {
+            if (duplicate >= maxNumberOfDuplications) {
+                quantityToBeDeleted++;
+            }
+        }
+        String[] result = new String[array.length - quantityToBeDeleted];
+        if (result.length == 0) return result;
+        int j=0;
+        for (int i=0; i < duplicates.length; i++){
+            if (duplicates[i] < maxNumberOfDuplications){
+                result[j] = array[i];
+                j++;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int indexToDelete = 0;
+        Integer[] duplicates = countOccurrences();
+        for (Integer duplicate : duplicates) {
+            if (duplicate == exactNumberOfDuplications) {
+                indexToDelete++;
+            }
+        }
+        String[] result = new String[array.length-indexToDelete];
+        int j=0;
+        for (int i=0; i < duplicates.length; i++){
+            if (duplicates[i] != exactNumberOfDuplications){
+                result[j] = array[i];
+                j++;
+            }
+        }
+        return result;
+    }
+
+    public Integer[] countOccurrences(){
+        Integer[] duplicatesCounter = new Integer[array.length];
+        for (int i=0; i<array.length; i++){
+            int counter = 1;
+            for (int j=0; j<array.length; j++){
+                if (i != j){
+                    if (array[i].equals(array[j])){
+                        counter++;
+                    }
+                }
+            }
+            duplicatesCounter[i] = counter;
+        }
+        return duplicatesCounter;
+    }
 }
